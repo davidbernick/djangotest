b5a4890ef9b5d6bead4d239511d6738c87eb4883
davidbernick <dbernick@gmail.com>
2014-06-21 11:46:45 -0400

    add commit

--- djangotest/polls/urls.py
+++ djangotest/polls/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth import views
 
 urlpatterns = patterns(
                        'polls.views',
+                       url(r'^logout/$', views.logout, {'next_page':'/polls/'},name='logout'),
                        url(r'^$', 'login_index', {'template_name':'signin.html'}, name='polls_index'),
 )
